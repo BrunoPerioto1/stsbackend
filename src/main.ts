3f28446042140ab9105ef03d970cb20f9dceb9d5
brunoperioto-gbmtech <bruno.perioto@gbmlogistica.com.br>
2025-09-04 16:51:33 -0300

    feat: configure CORS to allow requests from a specific origin

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,23 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  
+  // AQUI É ONDE VOCÊ MEXE.
+  // -------------------------------------------------------------
+  // Opção 1 (Recomendada): Permite requisições de uma origem específica
+  // Substitua 'https://SEU-FRONTEND.vercel.app' pela URL do seu front-end.
+  app.enableCors({
+    origin: 'https://sts-liart-alpha.vercel.app', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
+  
+  // Opção 2 (Temporária, para testes): Permite requisições de qualquer origem
+  // Essa abordagem é menos segura, mas resolve rapidamente o problema de CORS.
+  // app.enableCors();
+  // -------------------------------------------------------------
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
